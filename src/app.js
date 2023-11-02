7b0583ad54736d2ad16754cb0b866f5edbf7c35f
Davide Suzzarellu <147137494+DavideSuzzarellu@users.noreply.github.com>
2023-11-02 20:29:47 +0100

    app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"];
+
+  let whoExcusas = [Math.floor(Math.random() * who.length)];
+  let actionExcusas = [Math.floor(Math.random() * action.length)];
+  let whatExcusas = [Math.floor(Math.random() * what.length)];
+  let whenExcusas = [Math.floor(Math.random() * when.length)];
+
+  let excusaCompleta = who[whoExcusas] + " " + action[actionExcusas] + " " + what[whatExcusas] + " " + when[whenExcusas];
+  
+  document.getElementById("excuse").innerHTML = excusaCompleta;
 };
